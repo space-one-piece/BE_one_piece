ead127db4c35597daf3fe1f3362df8dd854291ae
PJG8806 <jinguy39@gmail.com>
2026-04-10 11:42:10 +0900

    🐛 fix      : 버그 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,4 +8,4 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-INTERNAL_IPS = ["127.0.0.1", "54.180.148.230", "kkugi.life"]
+INTERNAL_IPS = ["127.0.0.1", "54.180.148.230", "fragmnt.pics"]
