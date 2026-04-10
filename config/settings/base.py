c72399ba66058a875134e30c49a2a2e9a96254e9
PJG8806 <jinguy39@gmail.com>
2026-04-10 18:38:46 +0900

    🐛 fix      : 버그 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,9 +28,6 @@ if os.getenv("DJANGO_SETTINGS_MODULE") == "config.settings.local":
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-if not SECRET_KEY:
-    raise ValueError("DJANGO_SECRET_KEY not set")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = ["*"]
