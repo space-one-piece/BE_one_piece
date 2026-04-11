8d2cd62d45ed5f6f5067d409ca1470f87cb32308
PJG8806 <jinguy39@gmail.com>
2026-04-11 13:06:55 +0900

    🐛 fix      : 버그 수정

--- apps/question/views.py
+++ apps/question/views.py
@@ -1,7 +1,11 @@
 # Create your views here.
-
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class QuestAPIView(APIView):
-    pass
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return Response({"message": "Hello World!"})
