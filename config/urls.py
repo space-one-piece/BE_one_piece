28e4d0071c86c4986e34d924b9c20b75c4bb7e29
PJG8806 <jinguy39@gmail.com>
2026-04-11 12:19:47 +0900

    🐛 fix      : 버그 수정

--- config/urls.py
+++ config/urls.py
@@ -38,10 +38,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("admin", admin.site.urls),
-    path("docs", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("swagger", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("", RedirectView.as_view(url="/swagger", permanent=False)),
+    path("admin/", admin.site.urls),
+    path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("", RedirectView.as_view(url="/swagger/", permanent=False)),
     path("api/v1/question", include("apps.question.urls")),
 ]
 
