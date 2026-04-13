f04bb891894e15bcae80a9fb1ccaa3afd4dcdea1
PJG8806 <jinguy39@gmail.com>
2026-04-13 10:06:38 +0900

    🐛 fix       mypy 에러 수정
    # Conflicts:
    #	apps/question/views.py
    #	makefile

--- apps/question/views.py
+++ apps/question/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
 from rest_framework.permissions import AllowAny
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
