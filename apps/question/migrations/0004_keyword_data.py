21a25629663ac5577e245231f641c5b5178d2a8f
PJG8806 <jinguy39@gmail.com>
2026-04-16 13:47:08 +0900

    🐛 fix      : 버그 수정

--- apps/question/migrations/0004_keyword_data.py
+++ apps/question/migrations/0004_keyword_data.py
@@ -5,16 +5,14 @@ from django.db import migrations
 
 
 def load_all_data(apps, schema_editor):
-    # 1. 모델 가져오기
     Keyword = apps.get_model("question", "Keyword")
     Question = apps.get_model("question", "Question")
     Answer = apps.get_model("question", "QuestionsAnswer")
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    # JSON 파일들이 있는 루트 경로 (manage.py 위치)
+
     root_dir = os.path.normpath(os.path.join(current_dir, "../seed_data/"))
 
-    # --- [1] 키워드 데이터 로드 (keyword-seed-data.json) ---
     keyword_path = os.path.join(root_dir, "keyword-seed-data.json")
     if os.path.exists(keyword_path):
         with open(keyword_path, "r", encoding="utf-8") as f:
@@ -29,7 +27,6 @@ def load_all_data(apps, schema_editor):
                     },
                 )
 
-    # --- [2] 질문 데이터 로드 (questions-seed-data.json) ---
     question_path = os.path.join(root_dir, "question-seed-data.json")
     if os.path.exists(question_path):
         with open(question_path, "r", encoding="utf-8") as f:
@@ -39,7 +36,6 @@ def load_all_data(apps, schema_editor):
                     id=item["id"], defaults={"content": item["content"], "additional": item.get("additional", "")}
                 )
 
-    # --- [3] 답변 데이터 로드 (answers-seed-data.json) ---
     answer_path = os.path.join(root_dir, "answers-seed-data.json")
     if os.path.exists(answer_path):
         with open(answer_path, "r", encoding="utf-8") as f:
@@ -49,7 +45,7 @@ def load_all_data(apps, schema_editor):
                     q_obj = Question.objects.get(id=item["question_id"])
                     Answer.objects.get_or_create(
                         question=q_obj,
-                        answer=item["answer"],  # 모델 필드명이 answer인 경우
+                        answer=item["answer"],
                         defaults={"score": item["score"]},
                     )
                 except Question.DoesNotExist:
@@ -58,7 +54,7 @@ def load_all_data(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("question", "0003_question_additional"),  # 이전 마이그레이션 파일 확인
+        ("question", "0003_question_additional"),
     ]
 
     operations = [
