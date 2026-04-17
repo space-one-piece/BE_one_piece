650197fdc69051f5a1394efcea3b6e93b402c945
PJG8806 <jinguy39@gmail.com>
2026-04-17 14:29:23 +0900

    ✨ feat     : question 리뷰 입력 추가

--- apps/question/serializers/results_serializers.py
+++ apps/question/serializers/results_serializers.py
@@ -2,6 +2,21 @@ from typing import Any
 
 from rest_framework import serializers
 
+from apps.analysis.serializers.analysis_serializers import ScentDetailSerializer
+
 
 class ResultsSerializer(serializers.Serializer[dict[str, Any]]):
     temp = serializers.CharField()
+
+
+class ResultsIntSerializer(serializers.Serializer[dict[str, Any]]):
+    review = serializers.CharField()
+    rating = serializers.IntegerField(allow_null=True)
+
+
+class ResultsOutSerializer(serializers.Serializer[Any]):
+    id = serializers.IntegerField()
+    recommended_scent = ScentDetailSerializer(read_only=True)
+    reason = serializers.CharField()
+    review = serializers.CharField()
+    rating = serializers.IntegerField()
