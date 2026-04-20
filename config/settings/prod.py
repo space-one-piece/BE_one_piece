4ba57150c0cca0633c43cdbbfaa33c20e130e6dd
gyugyu99 <marsins90@gmail.com>
2026-04-20 14:19:17 +0900

    prod.py 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,3 +9,8 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 INTERNAL_IPS = ["127.0.0.1", "52.79.249.237", "fragmnt.pics"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://fragmnt.pics/api/docs/",
+]
