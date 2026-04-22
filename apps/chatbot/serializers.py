2db0d51f7ece71760a9984b457f41f5855aae95f
gyugyu99 <marsins90@gmail.com>
2026-04-22 16:46:14 +0900

    챗봇추천결과상세조회

--- apps/chatbot/serializers.py
+++ apps/chatbot/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from apps.analysis.models import Scent
+
 from .models import ChatbotRecommendation, ChatSession
 
 
@@ -17,3 +19,46 @@ class ChatbotRecommendationSerializer(serializers.ModelSerializer[ChatbotRecomme
     class Meta:
         model = ChatbotRecommendation
         fields = ["id", "scent_id", "retry_count", "is_saved", "saved_at", "created_at"]
+
+
+class ChatbotScentDetailSerializer(serializers.ModelSerializer[Scent]):
+    class Meta:
+        model = Scent
+        fields = [
+            "id",
+            "name",
+            "eng_name",
+            "description",
+            "categories",
+            "tags",
+            "keywords",
+            "intensity",
+            "is_bestseller",
+            "scent_notes",
+            "profile",
+            "season",
+            "recommended_places",
+            "similar_scents",
+            "thumbnail_url",
+            "created_at",
+        ]
+
+
+class ChatbotRecommendationDetailSerializer(serializers.ModelSerializer[ChatbotRecommendation]):
+    recommended_scent = ChatbotScentDetailSerializer(source="scent")
+    ai_comment = serializers.CharField(source="reply")
+    source_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ChatbotRecommendation
+        fields = [
+            "id",
+            "recommended_scent",
+            "ai_comment",
+            "source_type",
+            "is_saved",
+            "created_at",
+        ]
+
+    def get_source_type(self, obj: object) -> str:
+        return "chatbot"
