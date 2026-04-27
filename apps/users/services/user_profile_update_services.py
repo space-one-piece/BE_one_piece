c0c1b550538de17eb283265037af8d39fb31cf95
PJG8806 <jinguy39@gmail.com>
2026-04-27 14:40:18 +0900

    🐛 fix       사용자 프로필 이미지 출력 수정

--- apps/users/services/user_profile_update_services.py
+++ apps/users/services/user_profile_update_services.py
@@ -1,13 +1,17 @@
 from typing import Any
 
 from django.db import transaction
+from django.shortcuts import get_object_or_404
 
+from apps.core.utils.s3_handler import S3Handler
 from apps.users.models.models import User
 
 
 class UserProfileUpdateService:
     @staticmethod
-    def get_user_profile(user: User) -> User:
+    def get_user_profile(user_id: int) -> User:
+        user = get_object_or_404(User, id=user_id)
+        user.profile_image_url = S3Handler().s3_image(user.profile_image_url)
         return user
 
     @staticmethod
@@ -16,4 +20,6 @@ class UserProfileUpdateService:
         for attr, value in data.items():
             setattr(user, attr, value)
         user.save()
+
+        user.profile_image_url = S3Handler().s3_image(user.profile_image_url)
         return user
