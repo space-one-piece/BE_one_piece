b641eec5905c2e8faca93055375124a8fd044f44
PJG8806 <jinguy39@gmail.com>
2026-04-30 16:53:41 +0900

    🐛 fix      :web share 수정

--- apps/question/urls.py
+++ apps/question/urls.py
@@ -4,15 +4,11 @@ from rest_framework import routers
 from apps.question.views.image_user import ImageUserAPIView, UserImageAPIView
 from apps.question.views.keyword_views import KeywordAPIView
 from apps.question.views.quest_views import QuestAPIView
-from apps.question.views.results_views import (
-    ResultsCreateUrlAPIView,
-)
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path("survey", QuestAPIView.as_view(), name="question"),
-    path("web_share/<str:results_id>", ResultsCreateUrlAPIView.as_view(), name="web_share"),
     # path("results/<int:results_id>/review", ReviewViewAPIView.as_view(), name="review_results"),
     path("keyword", KeywordAPIView.as_view(), name="keyword"),
     path("image", ImageUserAPIView.as_view(), name="user_image"),
