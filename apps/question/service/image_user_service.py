c23b0d83fe6f9e748e9f588a2aea8743f84036a2
PJG8806 <jinguy39@gmail.com>
2026-04-30 13:34:12 +0900

    🐛 fix       이미지 생성 프롬프트 및 방식 변경

--- apps/question/service/image_user_service.py
+++ apps/question/service/image_user_service.py
@@ -1,11 +1,12 @@
 import base64
-import json
 
 from django.http import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from playwright.sync_api import sync_playwright
 
+from apps.analysis.models import ImageAnalysis
+from apps.chatbot.models import ChatbotRecommendation
 from apps.core.utils.cloud_front import image_url_cloud
 from apps.core.utils.s3_handler import image_url_edit
 from apps.question.google_ai_studio import Gemini
@@ -17,26 +18,52 @@ from apps.users.models.models import User
 class ImageUserService(QuestServices, Gemini):
     @classmethod
     def image_new(cls, user_id: int) -> str | None:
-        quest_data = get_list_or_404(QuestionsResults, user=user_id)
+        quest_data = QuestionsResults.objects.filter(user_id=user_id).order_by("created_at").first()
+        image_data = ImageAnalysis.objects.filter(user_id=user_id).order_by("created_at").first()
+        chat_data = ChatbotRecommendation.objects.filter(user_id=user_id).order_by("created_at").first()
 
-        if quest_data is None:
+        result = [obj for obj in [quest_data, image_data, chat_data] if obj is not None]
+
+        if not result:
             raise Http404
 
-        json_data = [json.loads(item.questions_json) for item in quest_data if item.questions_json]
+        last_data = max(result, key=lambda x: x.created_at)
+
+        category_data = {
+            "floral": "dusty rose, soft peony pink, pale lilac, creamy coral, hints of apricot",
+            "fruity": "sweet peach, pastel raspberry, soft mango yellow, pale strawberry pink, hints of apricot",
+            "woody": "soft sage green, warm taupe, dusty cedar, pale sand, muted amber, ivory",
+            "citrus": "pale lemon yellow, bright lime zest, soft aqua, misty white, sheer mandarin",
+            "musk": "milky white, soft pearl grey, ivory silk, misty lavender, creamy beige",
+            "green": "fresh mint, soft leaf green, pale pistachio, misty teal, dew-drop white",
+            "spicy": "muted terracotta, soft copper, warm cinnamon, dusty gold, creamy sand",
+            "powdery": "pale violet, soft baby pink, misty white, pearl silver, vanilla cream",
+            "aquatic": "soft ocean blue, misty cyan, pale turquoise, airy silver, sheer seafoam",
+            "amber": "warm honey gold, soft amber orange, muted bronze, deep cream, sunset glow",
+        }
+
+        if isinstance(last_data, ImageAnalysis):
+            last_sent_data = last_data.recommended_scent.eng_name if last_data.recommended_scent else ""
+            color_palette = category_data[last_data.recommended_scent.categories] if last_data.recommended_scent else ""
+        else:
+            last_sent_data = last_data.scent.eng_name if last_data.scent else ""
+            color_palette = category_data[last_data.scent.categories] if last_data.scent else ""
 
         prompt = f"""
-                {json_data}
-                with this data
-                dreamy pastel abstract background, soft blurred gradient, 
-                airy cloud-like color blending, very light pink, baby blue, 
-                ivory, pale lavender, subtle creamy tones, smooth and hazy transitions, 
-                ethereal atmosphere, minimal abstract wallpaper, soft focus, delicate glow, 
-                clean and bright, no sharp edges, no objects, no text, high resolution sharp lines, 
-                high contrast, vivid colors, strong shadows, detailed objects, flowers, people, text, 
-                patterns, noise, grain, geometric shapes
+                {last_sent_data}
+                t's perfect for this scent. Dreamy pastel abstract background, soft blurry gradation, 
+                Air cloud-like color blending, ({color_palette}), 
+                ivory, subtle creamy tones, soft and blurry transitions, subtle atmosphere, 
+                minimal abstract wallpaper, soft focus, delicate glow, clean and bright, no sharp edges, 
+                no objects, no text, high resolution clear lines.
+                [Negative Prompt]
+                high contrast, vivid colors, strong shadows, detailed objects, flowers, people, 
+                text, pattern, noise, particles, geometric shapes, watercolor splashes, paint splatter,
+                 water stains, watercolor texture, brush strokes, paint drips, liquid drips, bleeding colors, 
+                 messy edges, textured paper effect, drawing texture, sketchy lines, artistic mess.
             """
-        image_data = cls.image_gemini(prompt)
-        return image_data
+        generated_image_url: str | None = cls.image_gemini(prompt)
+        return generated_image_url
 
     @classmethod
     def user_profile_save(cls, user_id: int, image_url: str) -> None:
--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 from apps.analysis.views.scent_views import ScentDetailAPIView, ScentListCreateAPIView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     # 유저 어드민
     path("api/v1/admin/", include(("apps.users.urls.admin-urls", "admin"))),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
